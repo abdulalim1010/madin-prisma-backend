bfdf2c9afeebb00627b5d22710704e1be32bac49
abdulaim1010 <alim.12016032@student.brur.ac.bd>
2026-08-20 00:15:09 +0600

    start to the bkash  payment work

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, {
+	NextFunction,
 	type Application,
 	type Request,
 	type Response,
@@ -12,6 +13,8 @@ import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
 import { UserRoutes } from "./app/module/user/user.route";
+import { HttpStatus } from "http-status";
+import { getBkashIdToken } from "./app/lib/bkash";
 
 const app: Application = express();
 
@@ -53,6 +56,29 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 // ============================================
+
+
+//test route
+app.get("/test",async(req:Request,res:Response,next:NextFunction)=>{
+	try {
+
+		const grantIdTokenResult=await getBkashIdToken()
+		console.log(grantIdTokenResult)
+
+
+
+		res.status(httpStatus.OK).json({
+			success:true,
+			message:"welcome to the paymetn route",
+			data:null
+		});
+		
+	} catch (error) {
+		console.log(error)
+		next(error)
+		
+	}
+})
 // Not Found
 // ============================================
 app.use(notFound);
