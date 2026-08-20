965f6c71bff727fc495c11f8861085b82bf3823c
abdulaim1010 <alim.12016032@student.brur.ac.bd>
2026-08-20 10:30:11 +0600

    add to hte bkash token form the server on the rdis

--- src/app/lib/bkash.ts
+++ src/app/lib/bkash.ts
@@ -1,6 +1,57 @@
 import config from "../config"
+import { redisClient } from "./redis";
 
 export const getBkashIdToken=async()=>{
+
+
+try {
+    
+const IdTokenKey="bkash:IdToken"
+const RefreshTOkenKey="bkash:refreshToken"
+
+
+let bkashIdToken=await redisClient.get(IdTokenKey)
+
+
+let bkashRefreshToken=await redisClient.get(RefreshTOkenKey)
+
+if(!bkashIdToken && bkashRefreshToken){
+
+     const refreshTOkenResponse=await fetch(`${config.bkash_base_url}/tokenized/checkout/token/refresh`,
+        {
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                username:config.bkash_username,
+                password:config.bkash_password
+            },
+
+
+body:JSON.stringify(
+    {  
+   app_key: config.bkash_app_key,
+   app_secret: config.bkash_app_secret,
+   refresh_token:bkashRefreshToken
+}
+)
+
+
+        }
+
+
+    )
+
+
+    const bkashRefreshTokenResult=await refreshTOkenResponse.json()
+    bkashIdToken=bkashRefreshTokenResult.id_token
+    return bkashIdToken
+
+}
+
+if(bkashIdToken){
+    return bkashIdToken
+}
     const response=await fetch(`${config.bkash_base_url}/tokenized/checkout/token/grant`,
         {
             method:"POST",
@@ -26,6 +77,48 @@ body:JSON.stringify(
     )
 
 
-    const result=response.json();
-    return result
+  
+
+if(!response.ok){
+    throw new Error("Bkash access token grant faild");
+    
+}
+  const result=await response.json();
+
+ await redisClient.set(IdTokenKey,result.id_token,{
+    expiration:{
+        type:"EX",
+        value:60*60//one hours
+    }
+ })
+
+await redisClient.set(RefreshTOkenKey,result.refresh_token,{
+      expiration:{
+        type:"EX",
+        value:60*60*24*28//28days
+    }
+
+})
+
+
+bkashIdToken=result.id_token
+return bkashIdToken
+
+
+
+
+
+
+
+
+
+ //bksh refresh token
+
+
+    
+} catch (error:any) {
+    throw new Error(error.message);
+    
+    
+}
 }
